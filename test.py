b9ea080a3b5eca92d794bc76e6cf5203dab08714
PKUterran <swyang@pku.edu.cn>
2021-12-01 16:46:49 +0800

    211201-1645

--- test.py
+++
@@ -1,8 +0,0 @@
-from data.load_data import load_data
-
-a, b = load_data('sars', set_name='test', max_num=10)
-print(a[1].atom_features)
-print(a[1].bond_features)
-print(a[1].start_indices)
-print(a[1].end_indices)
-print(b[1])
